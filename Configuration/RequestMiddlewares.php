3eb4d8d022f3ac0892f46988744c9219841af56f
Yamen Hadla <yamen.hadla@visuellverstehen.de>
2023-03-17 16:17:46 +0100

    chore: do not middleware backend

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -4,23 +4,6 @@ return [
     'frontend' => [
         'middleware-identifier' => [
             'target' => \VV\T3languageredirection\Middleware\LanguageRedirectMiddleware::class,
-            'before' => [
-                'another-middleware-identifier',
-            ],
-            'after' => [
-                'yet-another-middleware-identifier',
-            ],
-        ],
-    ],
-    'backend' => [
-        'middleware-identifier' => [
-            'target' => \VV\T3languageredirection\Middleware\LanguageRedirectMiddleware::class,
-            'before' => [
-                'another-middleware-identifier',
-            ],
-            'after' => [
-                'yet-another-middleware-identifier',
-            ],
         ],
     ],
 ];
