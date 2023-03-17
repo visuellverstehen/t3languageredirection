e46da7893cb023fcca5f54c3428d0ad31b45506e
Yamen Hadla <yamen.hadla@visuellverstehen.de>
2023-03-17 13:55:07 +0100

    chore: initialize package file structure

---
+++ ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 't3languageredirection',
+    'description' => "This TYPO3 package provides a middleware that automatically redirects users to the preferred language URL based on their browser's Accept-Language header.",
+    'category' => 'be',
+    'author' => 'visuellverstehen',
+    'author_email' => 'hello@visuellverstehen.de',
+    'author_company' => 'visuellverstehen GmbH',
+    'state' => 'stable',
+    'clearCacheOnLoad' => false,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-11.5.99',
+        ]
+    ]
+];
