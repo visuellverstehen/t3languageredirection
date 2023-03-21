5f0463b3ece20fa0d8dce766febf80479ade9c0d
Malte Riechmann <kontakt@malteriechmann.de>
2023-03-21 17:24:46 +0100

    fix: change redirection logic, to break out redirect loop

--- Classes/Middleware/LanguageRedirectionMiddleware.php
+++ Classes/Middleware/LanguageRedirectionMiddleware.php
@@ -12,25 +12,37 @@ class LanguageRedirectionMiddleware implements MiddlewareInterface
 {
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
+        $requestParameters = $request->getAttribute('normalizedParams');
+
         // Do nothing, if the requested URL is not the root URL
-        if ($request->getServerParams()['REQUEST_URI'] !== '/') {
+        if ($requestParameters->getRequestUri() !== '/') {
             return $handler->handle($request);
         }
 
-        // Get the website's configured languages
+        // Do nothing, if a HTTP referer is set
+        if ($requestParameters->getHttpReferer() !== '') {
+            return $handler->handle($request);
+        }
+
+        // Get the website's configured site languages
         $siteLanguages = $request->getAttribute('site')->getLanguages();
 
-        // Do nothing, if the website has only one configured language
+        // Do nothing, if the website has only one configured site language
         if (count($siteLanguages) === 1) {
             return $handler->handle($request);
         }
 
-        // Get the browser language from the HTTP request headers
-        $browserLanguageIsoCode = substr($request->getHeaderLine('Accept-Language'), 0, 2);
+        // Get the is code of the browser language from HTTP request header
+        $browserLanguageIsoCode = substr($requestParameters->getHttpAcceptLanguage(), 0, 2);
 
         foreach ($siteLanguages as $siteLanguage) {
-            // Check if the browser language is supported
+            // Check if the browser language is supported by comparing two leter iso codes
             if ($browserLanguageIsoCode === $siteLanguage->getTwoLetterIsoCode()) {
+                // Do nothing, if the site language base URL is the currently requested URL
+                if ((string) $siteLanguage->getBase() === $requestParameters->getRequestUrl()) {
+                    return $handler->handle($request);
+                }
+
                 // Redirect the user to the preferred language base URL
                 return new RedirectResponse($siteLanguage->getBase());
             }
